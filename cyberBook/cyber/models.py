3050ba97895f828956cbe4894ded5064509c5f35
Alpha0531 <A01659734@itesm.mx>
2022-10-02 14:28:19 -0500

    Se añadieron los modelos

--- cyberBook/cyber/models.py
+++ cyberBook/cyber/models.py
@@ -1,3 +1,28 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Espacios(models.Model):
+    idEsp = models.CharField(primary_key=True, max_length=50, default=0)
+    nombreEspacio = models.CharField(max_length=30, default=0)
+    caracteristicas = models.CharField(max_length=50, default=0)
+    tiempoEsp = models.IntegerField()
+    disponibleEsp = models.BooleanField()
+    capacidad = models.IntegerField()
+    fechaEsp = models.CharField(max_length=30, default=0)
+
+class Softwares(models.Model):
+    idSoft = models.CharField(primary_key=True, max_length=50, default=0)
+    fechaSoft = models.CharField(max_length=30, default=0)
+    tipoSoft = models.CharField(max_length=20, default=0)
+    caracteristicas = models.CharField(max_length=50, default=0)
+    tiempoSoft = models.IntegerField()
+    disponibleSoft = models.BooleanField()
+
+class Reservas(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.CharField(max_length=30, default=0)
+    tipoRes = models.CharField(max_length=20, default=0)
+    numS = models.CharField(max_length=50, default=0)
+    estatus = models.BooleanField()
+    tiempoRes = models.IntegerField()
--- cyberBook/cyber/views.py
+++ cyberBook/cyber/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader 
 from .forms import NewUserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
